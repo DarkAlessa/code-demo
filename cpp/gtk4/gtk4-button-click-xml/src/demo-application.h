c343cab0aa2ae8c20f33e98dc60eb90849abd44b
DarkAlessa <skykick.013@gmail.com>
2024-06-07 14:34:28 +0700

    add new dir

---
+++ cpp/gtk4/gtk4-button-click-xml/src/demo-application.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <gtk/gtk.h>
+
+G_BEGIN_DECLS
+
+#define DEMO_APPLICATION_TYPE (demo_application_get_type())
+G_DECLARE_FINAL_TYPE(DemoApplication, demo_application, DEMO, APPLICATION, GtkApplication)
+
+DemoApplication *demo_application_new(void);
+
+G_END_DECLS
